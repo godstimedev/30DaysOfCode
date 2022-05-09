056045b1ec5f6cfb3ac2564bc9f29f584ca7ed8a
Godstime Agholor <agholorgodstime18@gmail.com>
2022-05-09 16:08:51 +0100

    new commit

--- day26-30/main.js
+++ day26-30/main.js
@@ -1,17 +1,6 @@
 const toggle = document.querySelector(".menu-icon");
 const menu = document.querySelector("#menu");
 
-function menutoggle() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove = "active";
-  } else {
-    menu.classList.add = "active";
-  }
-
-  console.log("you");
-}
-
-toggle.addEventListener("click", (e) => {
-  e.preventDefault();
-  menutoggle();
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
